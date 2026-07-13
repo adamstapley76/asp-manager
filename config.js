16c058a326f7182057af776ec69016f09890b6dd
adamstapley76 <adamstapley76@gmail.com>
2026-07-13 18:58:01 +0100

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.ASP_CONFIG = {
-  supabaseUrl: "https://your-project-id.supabase.co",
+  supabaseUrl: "https://njlxklvpbcmyaartboh.supabase.co",
   supabaseKey: "sb_publishable_meIRH_kySoqLaBuJlWeDEg_rf6RXquH"
 };
